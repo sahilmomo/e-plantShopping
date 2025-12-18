d1d5228f0d3fa5b823cbafb343fd378f95dbf1a2
Sahil Pal <sahilpalsitu@gmail.com>
2025-12-18 12:55:59 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Paradise Nursery is dedicated to providing healthy and affordable
+        houseplants. Our mission is to bring nature closer to your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
