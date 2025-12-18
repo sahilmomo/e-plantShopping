c494d5eae36dd0943bb175c3ba6673b28c4122ec
Sahil Pal <sahilpalsitu@gmail.com>
2025-12-18 13:06:48 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
